f215090d57be930a0b9bbe6969f4e1b7ed7efe2a
pod1um76 <lynant77@gmail.com>
2022-09-07 08:38:02 +0300

    Added methods for updating and deleting a person

--- src/main/java/ru/lynant/springlearn/dao/PersonDAO.java
+++ src/main/java/ru/lynant/springlearn/dao/PersonDAO.java
@@ -9,8 +9,7 @@ import java.util.List;
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
-
+    private final List<Person> people;
     PersonDAO() {
         people = new ArrayList<>();
 
@@ -19,18 +18,22 @@ public class PersonDAO {
         people.add(new Person(PEOPLE_COUNT++, "Mike"));
         people.add(new Person(PEOPLE_COUNT++, "Katy"));
     }
-
     public List<Person> getPeople() {
         return people;
     }
-
     public Person show(int id) {
         return people.get(id);
 
     }
-
     public void save(Person person) {
         person.setId(PEOPLE_COUNT++);
         people.add(person);
+    }
+    public void update(int id, Person updatedPerson) {
+        show(id).setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.remove(id);
     }
 }
