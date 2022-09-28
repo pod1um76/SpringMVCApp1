31d4abc419ce1adc88ae534ada5b04939d2615de
pod1um76 <lynant77@gmail.com>
2022-09-29 00:23:25 +0300

    Added batch update DB

---
+++ src/main/java/ru/lynant/springlearn/controllers/BatchController.java
@@ -0,0 +1,36 @@
+package ru.lynant.springlearn.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.lynant.springlearn.dao.PersonDAO;
+
+@Controller
+@RequestMapping("/test-batch-update")
+public class BatchController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public BatchController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping
+    public String index() {
+        return "batch/index";
+    }
+
+    @GetMapping("/without")
+    public String withoutBatch() {
+        personDAO.testMultipleUpdate();
+        return "redirect:/people";
+    }
+
+    @GetMapping("/with")
+    public String withBatch() {
+        personDAO.testBatchUpdate();
+        return "redirect:/people";
+    }
+}
