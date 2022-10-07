7479eba78abdacb813534ae4abff36c25936fd78
pod1um76 <lynant77@gmail.com>
2022-10-07 22:41:01 +0300

    Added validation for to repeat the email

--- src/main/java/ru/lynant/springlearn/controllers/PeopleController.java
+++ src/main/java/ru/lynant/springlearn/controllers/PeopleController.java
@@ -8,15 +8,18 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.lynant.springlearn.dao.PersonDAO;
 import ru.lynant.springlearn.models.Person;
+import ru.lynant.springlearn.util.PersonValidator;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping()
@@ -40,6 +43,9 @@ public class PeopleController {
     @PostMapping()
     public String create(@ModelAttribute ("person") @Valid Person person,
                          BindingResult bindingResult) {
+
+        personValidator.validate(person, bindingResult);
+
         if (bindingResult.hasErrors()) {
             return "people/new";
         }
@@ -58,6 +64,9 @@ public class PeopleController {
     public String update(@ModelAttribute ("person") @Valid Person person,
                                        BindingResult bindingResult,
                                        @PathVariable ("id") int id) {
+
+        personValidator.validate(person, bindingResult);
+
         if (bindingResult.hasErrors()) {
             return "people/edit";
         }
--- src/main/java/ru/lynant/springlearn/dao/PersonDAO.java
+++ src/main/java/ru/lynant/springlearn/dao/PersonDAO.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PersonDAO {
@@ -26,8 +27,13 @@ public class PersonDAO {
                 .stream().findAny().orElse(null);
     }
 
+    public Optional<Person> show(String email) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE email=?", new Object[] {email},
+                new PersonMapper()).stream().findAny();
+    }
+
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person(firstname, age, email) VALUES(?, ?, ?)", person.getName(), person.getAge(),
+        jdbcTemplate.update("INSERT INTO Person(firstName, age, email) VALUES(?, ?, ?)", person.getName(), person.getAge(),
                 person.getEmail());
     }
 
@@ -44,56 +50,56 @@ public class PersonDAO {
     ////Тест производительности пакетной вставки
     ////////////////////////////////////////////
 
-    public void testMultipleUpdate() {
-        List<Person> people = create1000People();
-
-        long before = System.currentTimeMillis();
-
-        for (Person person: people) {
-            jdbcTemplate.update("INSERT INTO Person VALUES(?, ?, ?, ?)", person.getName(), person.getAge(),
-                    person.getEmail());
-        }
-
-        long after = System.currentTimeMillis();
-
-        System.out.println("Time:" + (after - before));
-    }
-
-    public void testBatchUpdate() {
-        List<Person> people = create1000People();
-
-        long before = System.currentTimeMillis();
-
-       jdbcTemplate.batchUpdate("INSERT INTO Person VALUES (?, ?, ?, ?)",
-               new BatchPreparedStatementSetter() {
-                   @Override
-                   public void setValues(PreparedStatement ps, int i) throws SQLException {
-                       ps.setInt(1, people.get(i).getId());
-                       ps.setString(2, people.get(i).getName());
-                       ps.setInt(3, people.get(i).getAge());
-                       ps.setString(4, people.get(i).getEmail());
-                   }
-
-                   @Override
-                   public int getBatchSize() {
-                       return people.size();
-                   }
-               });
-
-        long after = System.currentTimeMillis();
-
-        System.out.println("Time:" + (after - before));
-    }
-
-    private List<Person> create1000People() {
-        List<Person> people = new ArrayList<>();
-
-        for (int i = 0; i < 1000; i++) {
-            people.add(new Person(i,
-                    "Name" + i,
-                    "test" + i + "@gmail.com",
-                    30));
-        }
-        return people;
-    }
+//    public void testMultipleUpdate() {
+//        List<Person> people = create1000People();
+//
+//        long before = System.currentTimeMillis();
+//
+//        for (Person person: people) {
+//            jdbcTemplate.update("INSERT INTO Person VALUES(?, ?, ?, ?)", person.getName(), person.getAge(),
+//                    person.getEmail());
+//        }
+//
+//        long after = System.currentTimeMillis();
+//
+//        System.out.println("Time:" + (after - before));
+//    }
+//
+//    public void testBatchUpdate() {
+//        List<Person> people = create1000People();
+//
+//        long before = System.currentTimeMillis();
+//
+//       jdbcTemplate.batchUpdate("INSERT INTO Person VALUES (?, ?, ?, ?)",
+//               new BatchPreparedStatementSetter() {
+//                   @Override
+//                   public void setValues(PreparedStatement ps, int i) throws SQLException {
+//                       ps.setInt(1, people.get(i).getId());
+//                       ps.setString(2, people.get(i).getName());
+//                       ps.setInt(3, people.get(i).getAge());
+//                       ps.setString(4, people.get(i).getEmail());
+//                   }
+//
+//                   @Override
+//                   public int getBatchSize() {
+//                       return people.size();
+//                   }
+//               });
+//
+//        long after = System.currentTimeMillis();
+//
+//        System.out.println("Time:" + (after - before));
+//    }
+//
+//    private List<Person> create1000People() {
+//        List<Person> people = new ArrayList<>();
+//
+//        for (int i = 0; i < 1000; i++) {
+//            people.add(new Person(i,
+//                    "Name" + i,
+//                    "test" + i + "@gmail.com",
+//                    30));
+//        }
+//        return people;
+//    }
 }
